e39e2beb685383b98c0efd23175264f8b1474d49
kali <kali@kali.netis.cc>
2024-04-26 16:31:06 +0300

    f coskjdfjhk

--- main.py
+++ main.py
@@ -294,7 +294,7 @@ class MAIN_CONTROLLER(MANAGER):
                 else:
                     if first_req_flag:
                         first_req_flag = False
-                        self.last_message.text = self.connector_func(self.last_message, "It is time to work!")                      
+                        self.last_message.text = self.connector_func(self.last_message, "It is time to work!")
 
                 start_data = ANNONCEMENT().bitget_parser()
                 # print(start_data)
