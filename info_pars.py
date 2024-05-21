0ce53ab20c962f85ad2116ce803b107fdacd06b5
kali <kali@kali.netis.cc>
2024-05-21 20:42:59 +0300

    403 err4

--- info_pars.py
+++ info_pars.py
@@ -19,25 +19,34 @@ user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
 ]
 
+# bitget_headers = {
+#     # 'accept': 'application/json, text/plain, */*',
+#     'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+#     'content-type': 'application/json;charset=UTF-8',
+#     'language': 'en_US',
+#     # 'locale': 'uk',
+#     'origin': 'https://www.bitget.com',
+#     # 'priority': 'u=1, i',
+#     'referer': 'https://www.bitget.com/support/',
+#     'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+#     # 'sec-ch-ua-mobile': '?0',
+#     # 'sec-ch-ua-platform': '"Linux"',
+#     'sec-fetch-dest': 'empty',
+#     'sec-fetch-mode': 'cors',
+#     'sec-fetch-site': 'same-origin',
+#     # 'terminaltype': '1',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+#     # 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    
+# }
+
 bitget_headers = {
-    # 'accept': 'application/json, text/plain, */*',
+    'accept': 'application/json, text/plain, */*',
     'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
     'content-type': 'application/json;charset=UTF-8',
-    'language': 'en_US',
-    # 'locale': 'uk',
     'origin': 'https://www.bitget.com',
-    # 'priority': 'u=1, i',
     'referer': 'https://www.bitget.com/support/',
-    'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-    # 'sec-ch-ua-mobile': '?0',
-    # 'sec-ch-ua-platform': '"Linux"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    # 'terminaltype': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
-    # 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-    
+    'User-Agent': choice(user_agents)
 }
 
 
