499dbec921f72426d82b2f64db320acbcdbf4fbb
kali <kali@kali.netis.cc>
2024-07-04 12:05:49 +0300

    s

--- SETTINGSS.py
+++ SETTINGSS.py
@@ -4,7 +4,7 @@ is_switch_on = True # бот включен/бот отключен
 class PARAMS():
     def __init__(self) -> None:
         self.railway_server_number = 1 # номер сервера
-        self.work_to = 23 # hoor in UTC до скольки работать
+        self.work_to = 17 # hoor in UTC до скольки работать
         self.sleep_to = 4 # hoor in UTC до скольки спать
         self.price_threshold = 5 # максимальная бидовая цена
         self.price_relation_threshold = 1.1 # минимальное отношение асков к бидам
